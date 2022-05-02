f54498cbe552665c898742c8866e491e14f36a82
Nature Nomad <76237641+naturenomad@users.noreply.github.com>
2022-05-02 16:37:07 +1200

    tidy up

--- natureclock.py
+++ natureclock.py
@@ -20,20 +20,16 @@
 import datetime
 import time
 from tkinter import *
-#from playsound import playsound
 from pydub import AudioSegment
 from pydub.playback import play
-import pyaudio # pyaudio isn't working
+import pyaudio
 from threading import Thread
 
 
 class NatureClock:
 
     def __init__(self, clock):
-        
-        #clock = Frame(master=master)
-        #clock.pack(expand=True, fill="both")
-        
+                
         clock.title("Nature Clock")
         clock.geometry("300x200")
         
@@ -66,7 +62,8 @@ class NatureClock:
         self.btStop = Button(clock, text="Stop", fg="black", width=10, command=self.stopClock)
         self.btStop.place(x=140, y=140)
     
-        
+    
+    # Preparing to introduce threading
     #def scanning(self):
     #    while True :   
     #        song = AudioSegment.from_mp3("98371_1635617-lq_englishcountryside.freesound.org.mp3") 
@@ -98,14 +95,11 @@ class NatureClock:
         # For a Tkinter.IntVar object - need to call its get method to access the value that it 
         # represents   
         self.active = True
-        print(self.active)
         wakeTime = "{}:{}".format(self.hour.get(), self.minute.get())
-        #print(wakeTime)
         while True :
             time.sleep(1)
             now = datetime.datetime.now().strftime("%H:%M")
             if now == wakeTime :
-                #playsound("98371_1635617-lq_englishcountryside.freesound.org.mp3")
                 song = AudioSegment.from_mp3("98371_1635617-lq_englishcountryside.freesound.org.mp3")
                 play(song)
                 break
@@ -118,7 +112,6 @@ class NatureClock:
 ## Compile the previous configuration and run the application
 root = Tk()
 clock = NatureClock(root)
-#root.protocol("WM_DELETE_WINDOW", handle_close)
 root.mainloop()
 
 
